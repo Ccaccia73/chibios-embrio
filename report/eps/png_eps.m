2b78ae6f5f8ca87d6be26879079dad4d208db158
Claudio Caccia <c.caccia@libero.it>
2012-09-15 18:15:11 +0200

    report tests and conclusions

--- report/eps/png_eps.m
+++ report/eps/png_eps.m
@@ -1,4 +1,4 @@
-img = imread('architecture.png');         %# A sample image
+img = imresize(imread('DSCF2211.JPG'),0.5);         %# A sample image
 imshow(img,'Border','tight',...      %# Display in a figure window without
        'InitialMagnification',100);  %#    a border at full magnification
-print('architecture.eps','-depsc2','-r300');
+print('DSCF2211.eps','-depsc2','-r300');
